6e69f29b8b89ee2a295d7c29b40717c068b8f4a9
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-04-27 16:18:42 +0200

    Create a new view controller. Configure the SceneDelegate to display the new view controller as the root view controller.

---
+++ SceneKit-Demo/Modules/Main/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  SceneKit-Demo
+//
+//  Created by Aitor Zubizarreta on 2023-04-27.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

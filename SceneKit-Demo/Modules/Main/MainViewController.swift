bc3fcb768127f50e80137e84682811edcb0ff0f6
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-04-27 17:49:50 +0200

    Add a method to create and display a scene programmatically.

--- SceneKit-Demo/Modules/Main/MainViewController.swift
+++ SceneKit-Demo/Modules/Main/MainViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SceneKit
 
 class MainViewController: UIViewController {
 
@@ -13,17 +14,41 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.white
+        
+        createScene()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func createScene() {
+        let sceneView = SCNView(frame: view.frame)
+        view.addSubview(sceneView)
+        
+        let scene = SCNScene()
+        sceneView.scene = scene
+        
+        // Camera.
+        let camera = SCNCamera()
+        let cameraNode = SCNNode()
+        cameraNode.camera = camera
+        cameraNode.position = SCNVector3(x: -3.0, y: 3.0, z: 3.0)
+        
+        // Light.
+        let light = SCNLight()
+        light.type = .omni
+        let lightNode = SCNNode()
+        lightNode.light = light
+        lightNode.position = SCNVector3(x: 1.5, y: 1.5, z: 1.5)
+        
+        // Cube.
+        let cubeGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        let cubeNode = SCNNode(geometry: cubeGeometry)
+        
+        let constraint = SCNLookAtConstraint(target: cubeNode)
+        constraint.isGimbalLockEnabled = true
+        cameraNode.constraints = [constraint]
+        
+        scene.rootNode.addChildNode(cameraNode)
+        scene.rootNode.addChildNode(lightNode)
+        scene.rootNode.addChildNode(cubeNode)
     }
-    */
-
+    
 }
